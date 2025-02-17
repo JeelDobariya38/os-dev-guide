6c09ddbfdbefa35e2940abe88758c00ee35e2ee1
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-02-17 21:23:06 +0530

    print a my simple os as message

--- Notes/file.asm
+++ Notes/file.asm
@@ -1,3 +1,4 @@
+bits 16
 org 0x7c00
 
 mov ah, 0x0e ; teletype mode
@@ -15,7 +16,7 @@ quit:
     jmp $
 
 data:
-    db 'Hello, World!!!', 0
+    db "My Simple OS!!!", 0
 
 times 510 - ($-$$) db 0
 dw 0xaa55
